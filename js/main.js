1e47b3208fecb786266cb0a20cd8a79b5efc423f
Sulaimon Yusuf <codewithemperor@gmail.com>
2024-11-06 17:23:22 +0100

    modified:   about.html 	modified:   contact.html 	modified:   css/responsive.css 	modified:   css/style.css 	new file:   img/destination/australia.jpg 	new file:   img/destination/canada.jpg 	new file:   img/destination/destination-details.jpg 	new file:   img/destination/destination-inner-1.jpg 	new file:   img/destination/destination_1_1.jpg 	new file:   img/destination/destination_1_2.jpg 	new file:   img/destination/destination_1_3.jpg 	new file:   img/destination/destination_1_4.jpg 	new file:   img/destination/destination_1_5.jpg 	new file:   img/destination/destination_2_1.jpg 	new file:   img/destination/destination_2_2.jpg 	new file:   img/destination/destination_2_3.jpg 	new file:   img/destination/destination_2_4.jpg 	new file:   img/destination/destination_3_1.jpg 	new file:   img/destination/destination_3_2.jpg 	new file:   img/destination/destination_3_3.jpg 	new file:   img/destination/destination_3_4.jpg 	new file:   img/destination/destination_4_1.jpg 	new file:   img/destination/destination_4_2.jpg 	new file:   img/destination/destination_4_3.jpg 	new file:   img/destination/destination_4_4.jpg 	new file:   img/destination/destination_4_5.jpg 	new file:   img/destination/destination_4_6.jpg 	new file:   img/destination/destination_4_7.jpg 	new file:   img/destination/destination_4_8.jpg 	new file:   img/destination/destination_5_1.jpg 	new file:   img/destination/destination_5_2.jpg 	new file:   img/destination/destination_5_3.jpg 	new file:   img/destination/destination_6_1.jpg 	new file:   img/destination/destination_6_2.jpg 	new file:   img/destination/destination_6_3.jpg 	new file:   img/destination/destination_6_4.jpg 	new file:   img/destination/destination_6_5.jpg 	new file:   img/destination/destination_7_1.jpg 	new file:   img/destination/destination_7_2.jpg 	new file:   img/destination/destination_7_3.jpg 	new file:   img/destination/destination_8_1.jpg 	new file:   img/destination/destination_8_2.jpg 	new file:   img/destination/destination_8_3.jpg 	new file:   img/destination/destination_9_1.jpg 	new file:   img/destination/destination_9_2.jpg 	new file:   img/destination/destination_9_3.jpg 	new file:   img/destination/destination_9_4.jpg 	new file:   img/destination/ireland.jpg 	new file:   img/destination/london.jpg 	new file:   img/destination/new-zealand.jpg 	new file:   img/destination/shape.png 	new file:   img/destination/united-states.jpg 	new file:   img/flyers/kunbis (1).png 	new file:   img/flyers/kunbis (2).png 	new file:   img/flyers/kunbis (3).png 	new file:   img/flyers/kunbis (4).png 	modified:   index.html 	modified:   js/main.js 	modified:   travel/index.html

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ AOS.init({
     duration: 500, // Duration of animation (in ms)
   });
 
+window.onscroll = function() {
+    const element = document.querySelector('.navbar');
+    const scrollPosition = window.scrollY; // Get the scroll position on the Y-axis
+
+    if (scrollPosition > 100) { // Adjust 100px to the point where you want to trigger the class
+        element.classList.add('fixed-top');
+
+    } else {
+        element.classList.remove('fixed-top');
+    }
+};
+
+
 
 // Index Swipper Our Service
 var swiper = new Swiper(".mySwiper", {
@@ -32,6 +45,53 @@ var swiper = new Swiper(".aboutUs", {
         disableOnInteraction: false,
       },
   });
+
+  var swiper = new Swiper(".destination-area", {
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 15,
+    
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
+
+//   Tour
+var swiper = new Swiper(".tour-area", {
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 15,
+    
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
+
+  var swiper = new Swiper(".flyers", {
+    effect: "cube",
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: true,
+      },
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
 
   
 
