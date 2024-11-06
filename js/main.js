e50d830360ba76c137e61fe1477a99a202b31068
Sulaimon Yusuf <codewithemperor@gmail.com>
2024-11-06 19:18:04 +0100

    modified:   about.html 	modified:   contact.html 	modified:   css/style.css 	deleted:    img/bg/line-pattern.png 	deleted:    img/bg/line-pattern2.png 	deleted:    img/bg/line-pattern3.png 	deleted:    img/logo-black.png 	deleted:    img/logo-light.png 	deleted:    img/original-16c045c6df6896aa7c11778c763af5ed.png 	deleted:    img/original-98716d020a334f3cbeb3cc4cc8f286db.png 	deleted:    img/original-e9405066287d3bc180962dc9cc1cf525.mp4 	deleted:    img/portfolio/1.jpg 	deleted:    img/portfolio/2.jpg 	deleted:    img/portfolio/3.jpg 	deleted:    img/portfolio/4.jpg 	deleted:    img/portfolio/5.jpg 	deleted:    img/portfolio/6.jpg 	deleted:    img/portfolio/7.jpg 	deleted:    img/portfolio/8.jpg 	modified:   index.html 	modified:   js/main.js 	modified:   travel/index.html

--- js/main.js
+++ js/main.js
@@ -7,12 +7,30 @@ window.onscroll = function() {
     const scrollPosition = window.scrollY; // Get the scroll position on the Y-axis
 
     if (scrollPosition > 100) { // Adjust 100px to the point where you want to trigger the class
+        element.classList.remove('position-absolute');
         element.classList.add('fixed-top');
 
     } else {
+        element.classList.add('position-absolute');
         element.classList.remove('fixed-top');
     }
 };
+
+
+var swiper = new Swiper(".bannerSwipper", {
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+  });
 
 
 
@@ -91,79 +109,3 @@ var swiper = new Swiper(".tour-area", {
       el: ".swiper-pagination",
     },
   });
-
-
-  
-
-/*---- HIDE / SHOW NAVBAR -----*/
-var ScrollPosition;
-var lastScrollPosition = 0;
-var delta = 5;
-var navbarHeight = $('#nav-bar').outerHeight();
-$(window).on("scroll", function (event) {
-    ScrollPosition = true;
-});
-setInterval(function () {
-    if (ScrollPosition) {
-        ScrollPosition = false;
-    }
-}, 250);
-
-
-/*---- PRELOADER PAGE -----*/
-paceOptions = {
-    ajax: true,
-    document: true,
-    eventLag: false
-};
-Pace.on('done', function () {
-    $('#preloader').addClass("isdone");
-    $('.loading-text').addClass("isdone");
-});
-
-
-
-$(document).ready(function () {
-    "use strict";
-
-    /*---- SCROLL BACK TO TOP -----*/
-    var progressPath = document.querySelector('.progress-wrap path');
-    var pathLength = progressPath.getTotalLength();
-    progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
-    progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
-    progressPath.style.strokeDashoffset = pathLength;
-    progressPath.getBoundingClientRect();
-    progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
-    var updateProgress = function () {
-        var scroll = $(window).scrollTop();
-        var height = $(document).height() - $(window).height();
-        var progress = pathLength - (scroll * pathLength / height);
-        progressPath.style.strokeDashoffset = progress;
-    }
-    updateProgress();
-    $(window).scroll(updateProgress);
-    var offset = 150;
-    var duration = 550;
-    jQuery(window).on('scroll', function () {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.progress-wrap').addClass('active-progress');
-        } else {
-            jQuery('.progress-wrap').removeClass('active-progress');
-        }
-    });
-    jQuery('.progress-wrap').on('click', function (event) {
-        event.preventDefault();
-        jQuery('html, body').animate({ scrollTop: 0 }, duration);
-        return false;
-    })
-});
-
-$(window).scroll(function () {
-
-    /*---- FADE SLIDESHOW -----*/
-    var scrolled = $(this).scrollTop();
-    $('.slider .caption').css({
-        'transform': 'translate3d(0, ' + -(scrolled * 0.20) + 'px, 0)',
-        'opacity': 1 - scrolled / 600
-    });
-});
